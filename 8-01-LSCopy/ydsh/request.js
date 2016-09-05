832be1855f600e59e0e704e60977a549cac5a048
Harry <18200166593@163.com>
2016-09-05 16:17:31 +0800

    2016-09-05
    2016-09-05 update

---
+++ 8-01-LSCopy/ydsh/request.js
@@ -0,0 +1,9 @@
+﻿
+function GetQueryString(name) {//获取url
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    }
+    return null;
+}
---
+++ 8-10-LSCopy/zcsll/request.js
@@ -0,0 +1,26 @@
+﻿
+function GetQueryString(name) {//获取url
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    }
+    return null;
+}
+
+function RegPhone(userPhone) {
+    var reg = /^[1][3-8]\d{9}$/;
+    if (!reg.test(userPhone)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+function ShowDiv(show_div) {
+    document.getElementById(show_div).style.display = 'block';
+};
+function CloseDiv(show_div) {
+    document.getElementById(show_div).style.display = 'none';
+};
