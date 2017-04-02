e6bd53a1abc87dc4b925af1cba313461243d9eeb
Alan <18200166593@163.com>
2017-04-02 14:40:09 +0800

    2017-04-02-14:40-home

--- bootstrap/test.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 2017-1-20.
- */
-var http = require('http');
-http.createServer(function(request,response){
-    response.writeHead(200,{'Content-Type':'text/plain'});
-    response.end('Hello,world\n');
-}).listen(8887);
-console.log('server runnig at http://127.0.0.1:8887/');
