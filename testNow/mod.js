44dc11facfdab427705269ff9d50bdcc53d3bb19
Alan <18200166593@163.com>
2017-05-27 16:47:15 +0800

    2017-05-27-16:47

---
+++ testNow/mod.js
@@ -0,0 +1,12 @@
+// mod.js
+const FOO_KEY = Symbol.for('foo');
+
+function A() {
+  this.foo = 'hello';
+}
+
+if (!global[FOO_KEY]) {
+  global[FOO_KEY] = new A();
+}
+
+module.exports = global[FOO_KEY];
