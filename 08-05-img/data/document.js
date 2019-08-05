a0df1564b7b9df8f734733f9745e1615f2cbf5e9
Harrylj <18200166593@163.com>
2019-08-05 18:49:47 +0800

    2019-08-05-18:49

---
+++ 08-05-img/data/document.js
@@ -0,0 +1,22 @@
+﻿$axure.loadDocument(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,_(c,d,e,f,g,d,h,d,i,d,j,k,l,d,m,f,n,f,o,f,p,f,q,[],r,f),s,_(t,[_(u,v,w,x,y,z,A,[_(u,B,w,x,y,C,A,[_(u,D,w,x,y,E)]),_(u,F,w,x,y,G),_(u,H,w,x,y,I)])]),J,_(K,L),M,_(N,O,P,_(Q,R,S,R),T,U),V,[],W,_(X,_(Y,Z,ba,bb,bc,bd,be,bf,bg,bh,bi,_(bj,bk,bl,bm,bn,bo),bp,bq,br,f,bs,bt,bu,bf,bv,_(bw,R,bx,R),P,_(Q,R,S,R),by,d,bz,f,bA,Z,bB,_(bj,bk,bl,bC),bD,_(bj,bk,bl,bE),bF,bG,bH,bk,bn,bG,bI,bJ,bK,bL,bM,bN,bO,bN,bP,bN,bQ,bN,bR,_(),bS,bJ,bT,bJ,bU,_(bV,f,bW,bX,bY,bX,bZ,bX,bl,_(ca,cb,cc,cb,cd,cb,ce,cf)),cg,_(bV,f,bW,R,bY,bX,bZ,bX,bl,_(ca,cb,cc,cb,cd,cb,ce,cf)),ch,_(bV,f,bW,bo,bY,bo,bZ,bX,bl,_(ca,cb,cc,cb,cd,cb,ce,ci))),cj,_(ck,_(Y,cl),cm,_(Y,cn,bF,bJ),co,_(Y,cp,bp,cq,bc,cr,bF,bJ,bB,_(bj,bk,bl,cs),bs,ct,bK,cu,bM,bJ,bO,bJ,bP,bJ,bQ,bJ),cv,_(Y,cw,bp,cx,bc,cr,bF,bJ,bB,_(bj,bk,bl,cs),bs,ct,bK,cu,bM,bJ,bO,bJ,bP,bJ,bQ,bJ),cy,_(Y,cz,bp,cA,bc,cr,bF,bJ,bB,_(bj,bk,bl,cs),bs,ct,bK,cu,bM,bJ,bO,bJ,bP,bJ,bQ,bJ),cB,_(Y,cC,bp,cD,bc,cr,bF,bJ,bB,_(bj,bk,bl,cs),bs,ct,bK,cu,bM,bJ,bO,bJ,bP,bJ,bQ,bJ),cE,_(Y,cF,bc,cr,bF,bJ,bB,_(bj,bk,bl,cs),bs,ct,bK,cu,bM,bJ,bO,bJ,bP,bJ,bQ,bJ),cG,_(Y,cH,bp,cI,bc,cr,bF,bJ,bB,_(bj,bk,bl,cs),bs,ct,bK,cu,bM,bJ,bO,bJ,bP,bJ,bQ,bJ),cJ,_(Y,cK,bF,bJ,bB,_(bj,bk,bl,cs),bs,ct,bK,cu,bM,bJ,bO,bJ,bP,bJ,bQ,bJ),cL,_(Y,cM,bi,_(bj,bk,bl,cN,bn,bo),bs,ct,bK,bL),cO,_(Y,cP,bB,_(bj,cQ,cR,[_(bl,bC),_(bl,cS),_(bl,cT),_(bl,bC)]))),cU,_()));}; 
+var b="configuration",c="showPageNotes",d=true,e="showPageNoteNames",f=false,g="showAnnotations",h="showAnnotationsSidebar",i="showConsole",j="linkStyle",k="displayMultipleTargetsOnly",l="linkFlowsToPages",m="linkFlowsToPagesNewWindow",n="hideAddress",o="preventScroll",p="useLabels",q="enabledViewIds",r="loadFeedbackPlugin",s="sitemap",t="rootNodes",u="pageName",v="index",w="type",x="Wireframe",y="url",z="index.html",A="children",B="detail",C="detail.html",D="order",E="order.html",F="list",G="list.html",H="screen",I="screen.html",J="globalVariables",K="onloadvariable",L="",M="defaultAdaptiveView",N="name",O="基本",P="size",Q="width",R=0,S="height",T="condition",U="<=",V="adaptiveViews",W="stylesheet",X="defaultStyle",Y="id",Z="627587b6038d43cca051c114ac41ad32",ba="fontName",bb="'Arial Normal', 'Arial'",bc="fontWeight",bd="400",be="fontStyle",bf="normal",bg="fontStretch",bh="5",bi="foreGroundFill",bj="fillType",bk="solid",bl="color",bm=0xFF333333,bn="opacity",bo=1,bp="fontSize",bq="13px",br="underline",bs="horizontalAlignment",bt="center",bu="lineSpacing",bv="location",bw="x",bx="y",by="visible",bz="limbo",bA="baseStyle",bB="fill",bC=0xFFFFFFFF,bD="borderFill",bE=0xFF797979,bF="borderWidth",bG="1",bH="linePattern",bI="cornerRadius",bJ="0",bK="verticalAlignment",bL="middle",bM="paddingLeft",bN="2",bO="paddingTop",bP="paddingRight",bQ="paddingBottom",bR="stateStyles",bS="rotation",bT="textRotation",bU="outerShadow",bV="on",bW="offsetX",bX=5,bY="offsetY",bZ="blurRadius",ca="r",cb=0,cc="g",cd="b",ce="a",cf=0.349019607843137,cg="innerShadow",ch="textShadow",ci=0.647058823529412,cj="customStyles",ck="box_1",cl="4b7bfc596114427989e10bb0b557d0ce",cm="_图片",cn="75a91ee5b9d042cfa01b8d565fe289c0",co="_一级标题",cp="1111111151944dfba49f67fd55eb1f88",cq="32px",cr="bold",cs=0xFFFFFF,ct="left",cu="top",cv="_二级标题",cw="b3a15c9ddde04520be40f94c8168891e",cx="24px",cy="_三级标题",cz="8c7a4c5ad69a4369a5f7788171ac0b32",cA="18px",cB="_四级标题",cC="e995c891077945c89c0b5fe110d15a0b",cD="14px",cE="_五级标题",cF="386b19ef4be143bd9b6c392ded969f89",cG="_六级标题",cH="fc3b9a13b5574fa098ef0a1db9aac861",cI="10px",cJ="_文本段落",cK="4988d43d80b44008a4a415096f1632af",cL="text_field",cM="44157808f2934100b68f2394a66b2bba",cN=0xFF000000,cO="_流程形状",cP="df01900e3c4e43f284bafec04b0864c4",cQ="linearGradient",cR="colors",cS=0xFFF2F2F2,cT=0xFFE4E4E4,cU="duplicateStyles";
+return _creator();
+})());
+
+(function(doc, win) {
+  var docEl = doc.documentElement,
+    recalc = function() {
+      var clientWidth = docEl.clientWidth;
+      // docEl.style.fontSize = clientWidth * (100 / 375) + 'px';
+      docEl.style.fontSize = clientWidth * (100 / 750) + 'px';
+    };
+  if(doc.addEventListener) {
+    win.addEventListener('resize', recalc, false);
+  } else {
+    win.attachEvent('onresize', recalc);
+  }
+  recalc();
+})(document, window);
