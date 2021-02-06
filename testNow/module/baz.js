e2c79e63bb8b70525f8939847f35b3998f31bb32
Harrylj <648267773@qq.com>
2021-02-06 15:24:35 +0800

    年前

---
+++ testNow/module/baz.js
@@ -0,0 +1,5 @@
+// 导入完整的 "foo" 和 "bar" 模块
+import foo from "foo";
+import bar from "bar";
+console.log(bar.hello("rhino")); // Let me introduce: rhino 
+foo.awesome(); // LET ME INTRODUCE: HIPPO
---
+++ testNow/module/foo.js
@@ -0,0 +1,8 @@
+// 仅从 "bar" 模块导入 hello()
+import hello from "bar";
+var hungry = "hippo";
+
+function awesome() {
+	console.log(hello(hungry).toUpperCase());
+}
+export awesome;
